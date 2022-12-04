d8a7d670e1af746cea86237b80456ca217dc2cfd
Diegopj22 <dpj2202@gmail.com>
2022-12-04 18:08:37 +0100

    pruebas

--- ejercicio3.py
+++ ejercicio3.py
@@ -119,7 +119,70 @@ class Grafo:
                         arribo_c(cola, aux)
                     adyac = adyac.sig
 
- 
+#cada desvío puede tener múltiples puntos de entrada y salida
+
+
+    def barrido_profundidad(self, vertice):
+        """Realiza un barrido en profundidad del grafo."""
+        pila = Pila()
+        aux = self.inicio
+        while(aux is not None):
+            aux.visitado = False
+            aux = aux.sig
+        aux = self.inicio
+        while(aux is not None and aux.info != vertice):
+            aux = aux.sig
+        if(aux is not None):
+            aux.visitado = True
+            apilar(pila, aux)
+            while(not pila_vacia(pila)):
+                dato = desapilar(pila)
+                print(dato.info, dato.tipo)
+                adyac = dato.adyacentes.inicio
+                while(adyac is not None):
+                    aux = self.inicio
+                    while(aux is not None and aux.info != adyac.destino):
+                        aux = aux.sig
+                    if(aux is not None and not aux.visitado):
+                        aux.visitado = True
+                        apilar(pila, aux)
+                    adyac = adyac.sig
+
+#se deben cargar seis estaciones de trenes y doce cambios de agujas;
+
+
+    def barrido_anchura2(self, vertice):
+        """Realiza un barrido en anchura del grafo."""
+        cola = Cola()
+        aux = self.inicio
+        while(aux is not None):
+            aux.visitado = False
+            aux = aux.sig
+        aux = self.inicio
+        while(aux is not None and aux.info != vertice):
+            aux = aux.sig
+        if(aux is not None):
+            aux.visitado = True
+            arribo_c(cola, aux)
+            while(not cola_vacia(cola)):
+                dato = atencion_c(cola)
+                print(dato.info, dato.tipo)
+                adyac = dato.adyacentes.inicio
+                while(adyac is not None):
+                    aux = self.inicio
+                    while(aux is not None and aux.info != adyac.destino):
+                        aux = aux.sig
+                    if(aux is not None and not aux.visitado):
+                        aux.visitado = True
+                        arribo_c(cola, aux)
+                    adyac = adyac.sig
+
+
+
+
+
+
+
 
 
 
