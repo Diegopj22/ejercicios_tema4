5892d6072683c5c31c341bc1b9f23a04b83b7728
Diegopj22 <dpj2202@gmail.com>
2022-12-04 16:53:58 +0100

    huffman

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,53 +1,103 @@
-from arbol_binario import Arbol
+class nodoArbolHuffman(object):
+    
+    def __init__(self, info, valor):
+        self.izq = None
+        self.der = None
+        self.info = info
+        self.valor = valor
 
-tabla = [['Despejado', 0.22], ['Baja', 0.78]]
+class nodoArbol(object):
 
+    def __init__(self, info, nrr=None):
+        self.izq = None
+        self.der = None
+        self.info = info
+        self.nrr = nrr
+
+def insertar_nodo(raiz, dato, nrr=None):
+    if(raiz is None):
+        raiz = nodoArbol(dato, nrr)
+    else:
+        if(raiz.info > dato):
+            raiz.izq = insertar_nodo(raiz.izq, dato, nrr)
+        else:
+            raiz.der = insertar_nodo(raiz.der, dato, nrr)
+    return raiz
+
+def por_nivel(raiz):
+    cola = Cola()
+    arribo(cola, raiz)
+    while(not cola_vacia(cola)):
+        nodo = atencion(cola)
+        print(nodo.info)
+        if(nodo.izq is not None):
+            arribo(cola, nodo.izq)
+        if(nodo.der is not None):
+            arribo(cola, nodo.der)
+
+
+#TABLA DE FRECUENCIAS
+tabla = [['A', 0.09], ['B', 0.01], ['C', 0.03], ['D', 0.02], ['E', 0.12], ['G', 0.02], ['I', 0.05], ['L', 0.05], ['M', 0.02], ['N', 0.05], ['O', 0.06], ['P', 0.03], ['Q', 0.008], ['S', 0.03], ['T', 0.02], ['U', 0.03], ['V', 0.01], ['Espacio', 0.15], ['coma', 0.01]]
 dic = {}
 
+def como_comparo(elemento):
+    return elemento[1]
 
-def como_comparo(arbol):
-    return arbol.frecuencia
+def como_comparo_nodo(elemento):
+    return elemento.valor
 
+tabla.sort(key=como_comparo)
 
 bosque = []
 
-for info, frecuencia in tabla:
-    arbol = Arbol(info, frecuencia)
-    bosque.append(arbol)
+for elemento in tabla:
+    nodo = nodoArbolHuffman(elemento[0], elemento[1])
+    bosque.append(nodo)
 
-
-bosque.sort(key=como_comparo)
-# for arbol in bosque:
-#     print(arbol.info, arbol.frecuencia)
-
+for elemento in bosque:
+    print(elemento.info, elemento.valor)
+print()
 while(len(bosque) > 1):
-    arbol1 = bosque.pop(0)
-    arbol2 = bosque.pop(0)
-    nuevo_arbol = Arbol(frecuencia=arbol1.frecuencia+arbol2.frecuencia)
-    nuevo_arbol.izq = arbol1
-    nuevo_arbol.der = arbol2
-    bosque.append(nuevo_arbol)
-    bosque.sort(key=como_comparo)
-
-arbol_huffman = bosque[0]
-
-arbol_huffman.barrido_por_nivel_huff()
+    elemento1 = bosque.pop(0)
+    elemento2 = bosque.pop(0)
+    nodo = nodoArbolHuffman('', elemento1.valor+elemento2.valor)
+    nodo.izq = elemento1
+    nodo.der = elemento2
+    bosque.append(nodo)
+    bosque.sort(key=como_comparo_nodo)
 
 
-def generar_tabla(arbol, cadena='', dic=None):
-    if(arbol is not None):
-        if(arbol.izq is None):
-            dic[arbol.info] = cadena
-            # print(arbol.info, cadena)
+#por_nivel(bosque[0])
+
+def generar_tabla(raiz, cadena=''):
+    if(raiz is not None):
+        if(raiz.izq is None):
+            print(raiz.info, cadena)
         else:
             cadena += '0'
-            generar_tabla(arbol.izq, cadena, dic)
+            generar_tabla(raiz.izq, cadena)
             cadena = cadena[0:-1]
             cadena += '1'
-            generar_tabla(arbol.der, cadena, dic)
+            generar_tabla(raiz.der, cadena)
 
 
-generar_tabla(arbol_huffman, dic=dic)
+generar_tabla(bosque[0])
+
+def decodificar(cadena, arbol_huff):
+    cadena_deco = ''
+    raiz_aux = arbol_huff
+    pos = 0
+    while(pos < len(cadena)):
+        if(cadena[pos] == '0'):
+            raiz_aux = raiz_aux.izq
+        else:
+            raiz_aux = raiz_aux.der
+        pos += 1
+        if(raiz_aux.izq is None):
+            cadena_deco += raiz_aux.info
+            raiz_aux = arbol_huff
+        cadena_deco
+    return cadena_deco
 
 
 def codificar(cadena, dic):
@@ -56,30 +106,21 @@ def codificar(cadena, dic):
         cadena_cod += dic[caracter]
     return cadena_cod
 
-
-def decodificar(cadena_cod, arbol_huff):
-    cadena_deco = ''
-    arbol_aux = arbol_huff
-    pos = 0
-    while(pos < len(cadena_cod)):
-        if(cadena_cod[pos] == '0'):
-            arbol_aux = arbol_aux.izq
+def generar_diccionario(raiz, cadena=''):
+    if(raiz is not None):
+        if(raiz.izq is None):
+            dic[raiz.info] = cadena
         else:
-            arbol_aux = arbol_aux.der
-        pos += 1
-        if(arbol_aux.izq is None):
-            cadena_deco += arbol_aux.info
-            arbol_aux = arbol_huff
-        # cadena_deco
-    return cadena_deco
+            cadena += '0'
+            generar_diccionario(raiz.izq, cadena)
+            cadena = cadena[0:-1]
+            cadena += '1'
+            generar_diccionario(raiz.der, cadena)
 
 
-cadena = ["Despejado", "Baja"]
-cadena_cod = codificar(cadena, dic)
-# from sys import getsizeof
-# print(getsizeof(cadena_cod), getsizeof(b'0000011001101111010000000000000101100101101010101101000'))
+generar_diccionario(bosque[0])
 
-print(decodificar(cadena_cod, arbol_huffman))
+print(dic)
 
 
- 
+
